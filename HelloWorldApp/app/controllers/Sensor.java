94cd295fd8b5539f1bbd38c492d9c2471d3faee3
Daniel Brown <nartey_brown@yahoo.com>
2013-11-12 19:11:12 -0800

    initial commit for query data models

---
+++ HelloWorldApp/app/controllers/Sensor.java
@@ -0,0 +1,32 @@
+package controllers;
+
+public class Sensor implements ISensor {
+	public Sensor(){};
+	public Sensor(String deviceType,
+				  String sensorType){
+		this.deviceType = deviceType;
+		this.sensorType = sensorType;
+	}
+
+	public String getDeviceType() {
+		return deviceType;
+	}
+	public void setDeviceType(String deviceType) {
+		this.deviceType = deviceType;
+	}
+	public String getSensorType() {
+		return sensorType;
+	}
+	public void setSensorType(String sensorType) {
+		this.sensorType = sensorType;
+	}
+	
+	@Override
+	public String toString() {
+		return "Sensor [deviceType=" + deviceType + ", sensorType="
+				+ sensorType + ", getDeviceType()=" + getDeviceType()
+				+ ", getSensorType()=" + getSensorType() + "]";
+	}
+	String deviceType;
+	String sensorType;
+}
