83fbbd61f288d82df3c6d09dd7880d1f40ca7c53
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2013-11-12 21:12:04 -0800

    Minor change

--- app/controllers/JsonParser.java
+++ app/controllers/JsonParser.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -10,23 +11,28 @@ public class JsonParser
 {
    public static List<QueryResponse> toResponseList(JsonNode jsonRoot)
    {
-	  ArrayList<QueryResponse> retList = new ArrayList<QueryResponse>();
-	  //expecting an ArrayNode
-	  Iterator<JsonNode> jsonIter = jsonRoot.iterator();
-	  while(jsonIter.hasNext())
-	  {
-		  JsonNode currNode = jsonIter.next();
-		  QueryResponse currResp = toResponse(currNode);
-		  retList.add(currResp);
-	  }
+      ArrayList<QueryResponse> retList = new ArrayList<QueryResponse>();
+      //expecting an ArrayNode
+      Iterator<JsonNode> jsonIter = jsonRoot.iterator();
+      while(jsonIter.hasNext())
+      {
+         JsonNode currNode = jsonIter.next();
+         QueryResponse currResp = toResponse(currNode);
+         retList.add(currResp);
+      }
       return retList;
    }
-   
+
    public static QueryResponse toResponse(JsonNode jsonRoot)
    {
       //expecting ObjectNode
-	  QueryResponse qr = new QueryResponse();
-	  //Still unimplemented!!
-	  return qr;
+      QueryResponse qr = new QueryResponse();
+      Iterator<Map.Entry<String,JsonNode>> fieldIter = jsonRoot.fields();
+      while(fieldIter.hasNext())
+      {
+         Map.Entry<String,JsonNode> currEntry = fieldIter.next();
+         
+      }
+      return qr;
    }
 }
