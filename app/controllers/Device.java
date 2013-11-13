95a2d745da494ad601cbdeda9bddb1af5835988a
Daniel Brown <nartey_brown@yahoo.com>
2013-11-12 20:59:46 -0800

    move files from under HelloWorld dir to root dir

--- app/controllers/Device.java
+++ app/controllers/Device.java
@@ -1,6 +1,58 @@
 package controllers;
 
-public class Device 
-{
+public class Device implements IDevice {
+	public Device(){};
+	public Device(String deviceId,
+				  String deviceType,
+				  String deviceAgent,
+				  String deviceLocation)
+	{
+		this.deviceId 	 	= deviceId;
+		this.deviceType  	= deviceType;
+		this.deviceAgent 	= deviceAgent;
+		this.deviceLocation = deviceLocation;
+	}
+	
+	public String getDeviceId() {
+		return deviceId;
+	}
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+	public String getDeviceType() {
+		return deviceType;
+	}
+	public void setDeviceType(String deviceType) {
+		this.deviceType = deviceType;
+	}
+	public String getDeviceAgent() {
+		return deviceAgent;
+	}
+	public void setDeviceAgent(String deviceAgent) {
+		this.deviceAgent = deviceAgent;
+	}
+	public String getDeviceLocation() {
+		return deviceLocation;
+	}
+	public void setDeviceLocation(String deviceLocation) {
+		this.deviceLocation = deviceLocation;
+	}
 
+	
+	@Override
+	public String toString() {
+		return "Device [deviceId=" + deviceId + ", deviceType=" + deviceType
+				+ ", deviceAgent=" + deviceAgent + ", deviceLocation="
+				+ deviceLocation + ", getDeviceId()=" + getDeviceId()
+				+ ", getDeviceType()=" + getDeviceType()
+				+ ", getDeviceAgent()=" + getDeviceAgent()
+				+ ", getDeviceLocation()=" + getDeviceLocation() + "]";
+	}
+
+
+
+	String deviceId;
+	String deviceType;
+	String deviceAgent;
+	String deviceLocation;
 }
---
+++ app/controllers/ISensor.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.lang.*;
+
+/**
+ * @author danny brown
+ * 
+ * @responsibilities interface for the encapsulation of
+ *                   a sensor object returned from platform
+ *                   API calls
+ */
+public interface ISensor {
+	/*
+	 * Returns the sensor type of the sensor
+	 * 
+	 * @return the sensor type as a string
+	 */	
+	public String getSensorType();
+	/*
+	 * Returns the name of the device type for the sensor
+	 * 
+	 * @return the name of the agent as a string
+	 */		
+	public String getDeviceType();
+	/*
+	 * Sets the sensor type
+	 * 
+	 * @param sensorType	A String representation of the sensor type
+	 * 
+	 */		
+	public void setSensorType(String sensorType);
+	/*
+	 * Sets the name of the device agent device type that contains this sensor
+	 * 
+	 * @param deviceType	A String representation device type
+	 * 
+	 */		
+	public void setDeviceType(String deviceType);
+}
