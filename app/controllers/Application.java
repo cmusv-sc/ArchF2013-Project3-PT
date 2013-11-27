bc03e8a1c92493d95361447ae1c71c6c2c7e3b9b
Rob Black <robwblack@gmail.com>
2013-11-26 21:41:36 -0800

    Fixing main, index, and some css stuff.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,53 +12,16 @@ import play.mvc.*;
 
 import views.html.dashboard;
 import views.html.index;
+import views.html.sensors;
 
 public class Application extends Controller {
-
-//    private static final String URL = "http://einstein.sv.cmu.edu/sensors/10170102/1368568896000/temp/json";
-    private static final String URL = "http://einstein.sv.cmu.edu/sensors";
 
     public static Result index() {
         return ok(index.render());
     }
-
-    /*public static Promise<Result> index() {
-
-        Promise<WS.Response> response = WS.url(URL).get();
-        return response.map(new Function<WS.Response, Result>() {
-            @Override
-            public Result apply(WS.Response resp) throws Throwable {
-                return (Result) ok(resp.asJson());
-            }
-        });
-    }*/
-
-    public static Promise<Result> sensors() {
-        DynamicForm requestData = Form.form().bindFromRequest();
-        String id = requestData.get("id");
-        String sensorType = requestData.get("sensorType");
-        String timestamp = requestData.get("timestamp");
-        String reportType = requestData.get("reportType");
-
-        String requestURL = new StringBuilder()
-                .append(URL).append("/")
-                .append(id).append("/")
-                .append(timestamp).append("/")
-                .append(sensorType).append("/")
-                .append(reportType).toString();
-
-        System.out.println(requestURL); //Testing
-
-        Promise<WS.Response> response = WS.url(requestURL).get();
-        return response.map(new Function<WS.Response, Result>() {
-            @Override
-            public Result apply(WS.Response resp) throws Throwable {
-//                System.out.println(resp.asJson());
-//                return ok(index.render("Results: " + resp.asJson()));
-                return ok();
-            }
-        });
-
+    
+    public static Result sensors() {
+      return ok(sensors.render());
     }
 
     /**
