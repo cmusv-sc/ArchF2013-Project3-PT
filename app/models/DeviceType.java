536812ab3d6717282dd7fbaa83a8b0993b4175a4
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2013-11-29 20:24:24 -0800

    Add JavaDoc to DeviceType

--- app/models/DeviceType.java
+++ app/models/DeviceType.java
@@ -3,27 +3,54 @@ package models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Comparable representation of DeviceType
+ * A DeviceType contains a name identifer
+ * as well as a list of SensorTypes that it supports
+ * 
+ * @author Team Mercury
+ */
 public class DeviceType implements Comparable<DeviceType>
 {
+  /** the identfier for this DeviceType (ex: Firefly_V3) */
   private final String typeName;
+  /** the list of SensorTypes supported (ex: Temp, CO2) */
   private List<SensorType> sensors;
 
+  /**
+   * Constuctor - requires the typeName as an identifier
+   * @param typeName
+   */
   public DeviceType(String typeName)
   {
     this.typeName = typeName;
     this.sensors = new ArrayList<SensorType>();
   }
 
+  /**
+   * Add a SensorType to the list this DeviceType supports
+   * @param sensorType
+   */
   public void add(SensorType sensorType)
   {
     sensors.add(sensorType);
   }
 
+  /**
+   * Get the type identifer of this DeviceType
+   * (e.g. "Firefly_V3")
+   * @return the type as a String
+   */
   public String getType()
   {
     return typeName;
   }
 
+  /**
+   * Get the list of SensorTypes
+   * (e.g. "Temp", "CO2")
+   * @return list of supported SensorTypes
+   */
   public List<SensorType> getSensorTypes()
   {
     return sensors;
