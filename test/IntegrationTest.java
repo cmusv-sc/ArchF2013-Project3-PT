c184f365c28be38521d496f45e502120da314c35
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2013-11-29 18:42:44 -0800

    Remove ApplicationTest and format IntegrationTest

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,28 +1,36 @@
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
-import static org.fluentlenium.core.filter.FilterConstructor.*;
-
-public class IntegrationTest {
-
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
-    @Test
-    public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
-            }
-        });
-    }
+/**
+ * Contains Integration tests - for testing proper html rendering
+ * @author Team Mercury
+ */
+@RunWith(JUnit4.class)
+public class IntegrationTest
+{
+   /**
+    * simple integration test to check if the index page is being shown
+    */
+   @Test
+   public void simpleIndexTest()
+   {
+      running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT,
+            new Callback<TestBrowser>()
+            {
+               public void invoke(TestBrowser browser)
+               {
+                  browser.goTo("http://localhost:3333");
+                  assertThat(browser.pageSource()).contains(
+                        "Team Mercury");
+               }
+            });
+   }
 
 }
